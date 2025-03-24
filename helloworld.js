33079ec6d2b469c853026ddc4e610b26404c8653
sumanthupar <43923464+sumanthupar@users.noreply.github.com>
2025-03-24 14:09:13 +0530

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type' : 'text/plain'});
-  res.end('Hello from JFROGs12345\n');
+  res.end('Hello from JFROGs\n');
 }).listen(1337, '127.0.0.1');
